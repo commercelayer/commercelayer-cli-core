784550975f6cbb8814d291f04be99a3e522872d3
Pierluigi Viti <pierluigi@commercelayer.io>
2024-04-09 13:43:56 +0200

    feat(text): add dasherize function

--- src/text.ts
+++ src/text.ts
@@ -1,15 +1,21 @@
 import Inflector from './inflector'
 
 
-const capitalize = (str: string | undefined): string | undefined => {
+const capitalize = (str?: string): string | undefined => {
 	if (!str) return str
 	return Inflector.capitalize(str)
 }
 
-const pluralize = (str: string, plural?: string | undefined): string => { return Inflector.pluralize(str, plural) }
-const singularize = (str: string, singular?: string | undefined): string => { return Inflector.singularize(str, singular) }
-const camelize = (str: string, lowFirstLetter?: boolean | undefined): string => { return Inflector.camelize(str, lowFirstLetter) }
+const dasherize = (str?: string): string | undefined => {
+	if (!str) return str
+    str = str.toLocaleLowerCase()
+		return str.replace(/ _/g, '-')
+}
+
+const pluralize = (str: string, plural?: string): string => { return Inflector.pluralize(str, plural) }
+const singularize = (str: string, singular?: string): string => { return Inflector.singularize(str, singular) }
+const camelize = (str: string, lowFirstLetter?: boolean): string => { return Inflector.camelize(str, lowFirstLetter) }
 
 
-export { capitalize, pluralize, singularize, camelize }
+export { capitalize, pluralize, singularize, camelize, dasherize }
 export { symbols } from './symbol'
