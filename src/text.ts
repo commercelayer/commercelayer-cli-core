df4f5aa311764faae8badc6c5ed37ab51f9a05bd
Pierluigi Viti <pierluigi@commercelayer.io>
2024-08-12 15:48:54 +0200

    feat: add underscorize function

--- src/text.ts
+++ src/text.ts
@@ -12,10 +12,17 @@ const dasherize = (str?: string): string | undefined => {
 		return str.replace(/ _/g, '-')
 }
 
+const underscorize = (str?: string): string | undefined => {
+	if (!str) return str
+    str = str.toLocaleLowerCase()
+		return str.replace(/ -/g, '_')
+}
+
 const pluralize = (str: string, plural?: string): string => { return Inflector.pluralize(str, plural) }
 const singularize = (str: string, singular?: string): string => { return Inflector.singularize(str, singular) }
 const camelize = (str: string, lowFirstLetter?: boolean): string => { return Inflector.camelize(str, lowFirstLetter) }
 
 
-export { capitalize, pluralize, singularize, camelize, dasherize }
+
+export { capitalize, pluralize, singularize, camelize, dasherize, underscorize }
 export { symbols } from './symbol'
